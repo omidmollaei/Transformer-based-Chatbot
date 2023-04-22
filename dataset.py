d2056b441f98b02bb1f9126c3ad88114e16f6d80
unknown <omidmollaei2002@gmail.com>
2023-04-22 21:28:40 +0430

    add a function to remove contractions from a given sentence.

--- dataset.py
+++ dataset.py
@@ -1,5 +1,30 @@
 """ Load and prepare Cornell Movie Dialog dataset , a famous dataset for training a chatbot model."""
 
+import os
+import re
+import dataclasses
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from tqdm import tqdm
 
 
-
+def remove_contractions(sentence: str) -> str:
+    sentence = re.sub(r"i'm", "i am", sentence)
+    sentence = re.sub(r"he's", "he is", sentence)
+    sentence = re.sub(r"she's", "she is", sentence)
+    sentence = re.sub(r"it's", "it is", sentence)
+    sentence = re.sub(r"that's", "that is", sentence)
+    sentence = re.sub(r"what's", "what is", sentence)
+    sentence = re.sub(r"where's", "where is", sentence)
+    sentence = re.sub(r"how's", "how is", sentence)
+    sentence = re.sub(r"\'ll", " will", sentence)
+    sentence = re.sub(r"\'ve", " have", sentence)
+    sentence = re.sub(r"\'re", " are", sentence)
+    sentence = re.sub(r"\'d", " would", sentence)
+    sentence = re.sub(r"\'re", " are", sentence)
+    sentence = re.sub(r"won't", "will not", sentence)
+    sentence = re.sub(r"can't", "cannot", sentence)
+    sentence = re.sub(r"n't", " not", sentence)
+    sentence = re.sub(r"n'", "ng", sentence)
+    sentence = re.sub(r"'bout", "about", sentence)
+    return sentence
