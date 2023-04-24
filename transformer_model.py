584d101f844c74ad3584169ce4c62fc62c4a8e53
unknown <omidmollaei2002@gmail.com>
2023-04-24 13:00:28 +0430

    define a function to build one block of encoder.

--- transformer_model.py
+++ transformer_model.py
@@ -135,3 +135,27 @@ class MultiHeadAttentionLayer(tf.keras.layers.Layer):
         return outputs
 
 
+def encoder_layer(params: ModelHp, name: str = "encoder_layer"):
+    inputs = tf.keras.layers.Input(shape=(None, params.d_model), name="inputs")
+    padding_mask = tf.keras.layers.Input(shape=(1, 1, None), name="padding_mask")
+
+    attention = MultiHeadAttentionLayer(
+        num_heads=params.num_attention_heads, model_dim=params.d_model, name="attention"
+    )({
+        "query": inputs,
+        "key": inputs,
+        "value": inputs,
+        "mask": padding_mask
+    })
+    attention = tf.keras.layers.Dropout(params.dropout_rate)(attention)
+    attention += tf.cast(inputs, dtype=tf.float32)
+    attention = tf.keras.layers.LayerNormalization(epsilon=1e-6)(attention)
+
+    outputs = tf.keras.layers.Dense(params.num_units, activation=params.activation)(attention)
+    outputs = tf.keras.layers.Dense(params.d_model)(outputs)
+    outputs = tf.keras.layers.Dropout(params.dropout_rate)(outputs)
+    outputs += attention
+    outputs = tf.keras.layers.LayerNormalization(epsilon=1e-6)(outputs)
+    return tf.keras.Model(inputs=[inputs, padding_mask], outputs=outputs, name=name)
+
+
