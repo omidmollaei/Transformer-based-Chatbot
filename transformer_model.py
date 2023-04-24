d02bf7975cedd57747049c8ebe566f9dd68978aa
unknown <omidmollaei2002@gmail.com>
2023-04-24 11:37:46 +0430

    write a funciton to create look ahead mask (to prevent decoder paying attention to forward tokens)

--- transformer_model.py
+++ transformer_model.py
@@ -21,3 +21,13 @@ class ModelHp:
 def create_padding_mask(inputs: tf.Tensor):
     mask = tf.cast(tf.math.equal(inputs, 0), tf.float32)
     return mask[:, tf.newaxis, tf.newaxis, :]
+
+
+def create_look_ahead_mask(inputs: tf.Tensor):
+    seq_len = tf.shape(inputs)[1]
+    mask = tf.ones((seq_len, seq_len), dtype=tf.float32)
+    mask = 1 - tf.linalg.band_part(mask, -1, 0)
+    padding_mask = create_padding_mask(inputs)
+    return tf.maximum(mask, padding_mask)
+
+
